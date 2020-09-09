f8ea5158dd42ecc4c32c2e92c4526b646a75b855
Morgen Lanueva <morgen.lanueva@gmail.com>
2020-09-08 23:35:45 -0500

    servlet and controller work

---
+++ pOne/src/main/java/com/revature/controller/AuthenticateController.java
@@ -0,0 +1,20 @@
+package com.revature.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticateController {
+
+	public static void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Login Poggers");
+		
+		System.out.println(request.getParameter("username"));
+	}
+	
+	public static void logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Logout Poggers");
+	}
+}
