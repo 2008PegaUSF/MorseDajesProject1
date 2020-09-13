1f87cb42735a70b9b8cc4109df4f1a9b436bcbfe
Morgen Lanueva <morgen.lanueva@gmail.com>
2020-09-13 01:34:46 -0500

    file upload functionality included

--- ProjectOne/src/main/java/com/revature/controller/EmployeeController.java
+++ ProjectOne/src/main/java/com/revature/controller/EmployeeController.java
@@ -5,7 +5,11 @@ import java.io.InputStream;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.servlet.http.Part;
 import org.apache.commons.io.IOUtils;
 import javax.servlet.annotation.MultipartConfig;
@@ -18,10 +22,10 @@ import javax.servlet.http.HttpSession;
 import com.revature.daoimpl.RequestsDaoImpl;
 import com.revature.util.ConnFactory;
 
-@javax.servlet.annotation.MultipartConfig
+@MultipartConfig
 public class EmployeeController {
 
-	public static void getRequestForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public static synchronized void getRequestForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		HttpSession sesh = request.getSession(false);
 		if (sesh == null) {
@@ -48,7 +52,43 @@ public class EmployeeController {
 			
 			RequestsDaoImpl rdi = new RequestsDaoImpl();
 			try {
+				 ConnFactory cf = ConnFactory.getInstance();
+				 Connection conn = cf.getConnection();
+				 
+				//Get the requestid of the request we are about to submit (to add to the requestfiles table if necessary)
+				PreparedStatement pr = conn.prepareStatement("select nextval('requests_requestid_seq') from requests;");
+				ResultSet rs = pr.executeQuery();
+				rs.next();
+				int requestid = rs.getInt(1);
+				
+				//Reset sequence value after query
+				PreparedStatement prep = conn.prepareStatement("SELECT SETVAL((SELECT pg_get_serial_sequence('requests', 'requestid')), ?, false);");
+				prep.setInt(1, requestid);
+				prep.executeQuery();
+				
+				//Create request
 				rdi.createRequest(location, description, Double.parseDouble(cost), gradingFormat, eventType, userid, justification, time, date, firstName, lastName);
+			
+				// Retrieves files from <input type="file" name="file" multiple> 
+				 List<Part> fileParts = request.getParts().stream().filter(part -> "file".equals(part.getName())).collect(Collectors.toList());
+				 
+				    for (Part filePart : fileParts) {
+				    	if (filePart != null) {
+					        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+						
+					        //Create InputStream and convert to byte[]
+					        InputStream fileStream = filePart.getInputStream();
+					        byte[] bytes = IOUtils.toByteArray(fileStream);
+					        
+					    	PreparedStatement ps = conn.prepareStatement("INSERT INTO requestfiles VALUES (?, ?, ?)");
+							ps.setInt(1, requestid);
+							ps.setString(2, fileName);
+							ps.setBytes(3, bytes);
+							ps.executeUpdate();
+							ps.close();
+				    	}
+				    } 
+			
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
