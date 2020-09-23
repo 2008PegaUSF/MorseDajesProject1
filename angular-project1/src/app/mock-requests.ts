1466f02f2ebca034262719848b5cbb621cc8ed2d
David <daviddraper97@yahoo.com>
2020-09-23 16:42:26 -0400

    Implemented hard-coded requests

---
+++ angular-project1/src/app/mock-requests.ts
@@ -0,0 +1,42 @@
+import { Request } from "./request";
+
+export const REQUESTS: Request[] = [
+    {
+        id: 1,
+        eventDate: "2020-10-30",
+        eventTime: "13:00pm",
+        location: "USF",
+        description: "Revature Training",
+        cost: 2700,
+        gradingFormat: "Grade",
+        eventType: "Certification",
+        userid: 1,
+        justification: "To launch my software development career",
+        firstName: "Dave",
+        lastName: "Draper",
+        projectedAmount: 1800,
+        awardedAmount: 0,
+        status: "pending"
+    }
+]
+
+/*
+    ,
+    {
+        id: 0,
+        eventDate: "",
+        eventTime: "",
+        location: "",
+        description: "",
+        cost: 0,
+        gradingFormat: "",
+        eventType: "",
+        userid: 0,
+        justification: "",
+        firstName: "",
+        lastName: "",
+        projectedAmount: 0,
+        awardedAmount: 0,
+        status: ""
+    }
+ */
--- angular-project1/src/app/view-my-requests/view-my-requests.component.ts
+++ angular-project1/src/app/view-my-requests/view-my-requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { REQUESTS } from "../mock-requests";
 
 @Component({
   selector: 'app-view-my-requests',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-my-requests.component.css']
 })
 export class ViewMyRequestsComponent implements OnInit {
+
+  requests = REQUESTS;
 
   constructor() { }
 
