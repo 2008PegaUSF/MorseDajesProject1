7a0ea2da1588cca99c347f8bac0e0bc5f068f0d0
JesseTWarren <69748727+JesseTWarren@users.noreply.github.com>
2020-09-23 19:17:28 -0400

    Add files via upload

---
+++ angular-project1/mock-users.ts
@@ -0,0 +1,6 @@
+export const USERS: User[] = [
+    { UserId:1, firstName:'Jesse',lastName:'Warren',userType:'Employee',balance: 0,reportsto: 2,email:'jtwarrenz@gmail.com'},
+    { UserId:2, firstName:'Dave',lastName:'Draper',userType:'Supervisor',balance: 0,reportsto: 3,email:'dirtydave@yahoo.com'},
+    { UserId:3, firstName:'Seth',lastName:'Pennebaker',userType:'Department Head',balance: 0,reportsto: 4, email:'pennepasta@hotmail.com'},
+    { UserId:4, firstName:'Morgan',lastName:'Lanueva',userType:'Benefits Coordinator',balance: 0,reportsto: null,email:'Venmoyourbenco@gmail.com'},
+  ];
