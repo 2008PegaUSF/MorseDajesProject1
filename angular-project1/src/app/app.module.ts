043a2403428f7c9d81b801a3a041e38530350e36
Morgen Lanueva <morgen.lanueva@gmail.com>
2020-09-23 00:58:38 -0500

    Components populated

--- angular-project1/src/app/app.module.ts
+++ angular-project1/src/app/app.module.ts
@@ -3,32 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BackgroundComponent } from './background/background.component';
 import { LoginComponent } from './login/login.component';
 import { EmployeeTabsComponent } from './employee-tabs/employee-tabs.component';
-import { SupervisorabtsComponent } from './supervisorabts/supervisorabts.component';
-import { InfoComponent } from './info/info.component';
 import { SubmitMyRequestComponent } from './submit-my-request/submit-my-request.component';
 import { ViewMyRequestsComponent } from './view-my-requests/view-my-requests.component';
 import { UploadMyGradeComponent } from './upload-my-grade/upload-my-grade.component';
 import { PendingRequestsComponent } from './pending-requests/pending-requests.component';
 import { PendingGradesComponent } from './pending-grades/pending-grades.component';
 import { Employee101Component } from './employee101/employee101.component';
+import { Supervisor101Component } from './supervisor101/supervisor101.component';
+import { SupervisorTabsComponent } from './supervisor-tabs/supervisor-tabs.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundComponent,
     LoginComponent,
     EmployeeTabsComponent,
-    SupervisorabtsComponent,
-    InfoComponent,
     SubmitMyRequestComponent,
     ViewMyRequestsComponent,
     UploadMyGradeComponent,
     PendingRequestsComponent,
     PendingGradesComponent,
-    Employee101Component
+    Employee101Component,
+    Supervisor101Component,
+    SupervisorTabsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
