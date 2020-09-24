abebe83afb9481a2361d8bb22d66e26253db2857
David <daviddraper97@yahoo.com>
2020-09-24 17:10:05 -0400

    Implemented Non-Dynamic Cancellation, Approval, and Denial

--- angular-project1/src/app/pending-requests/pending-requests.component.ts
+++ angular-project1/src/app/pending-requests/pending-requests.component.ts
@@ -10,22 +10,72 @@ import { RequestService } from "../request.service";
 })
 export class PendingRequestsComponent implements OnInit {
 
-  status: string = "pending";
+  // Fields
+  requests: Request[];
+  filteredRequests: Request[];
+  selected: Request[];
+  action: string;
 
-  performFilter(userid: number): Request[]{
+  // Filter by Request Status
+  performFilter(status: string): Request[]{
   
     return this.requests.filter(
 
-      (request: Request) => request.status == this.status
+      (request: Request) => request.status == status
     );
   }
 
-  requests: Request[];
-  filteredRequests: Request[];
+  // Activate when the checkbox is flipped
+  FieldsChange(values:any,id:number){
+    if (values.currentTarget.checked) {
+      this.selected.push(
+        this.requestService.get(id)
+      );
+    } else {
+      this.selected.splice(
+        this.selected.indexOf(this.requestService.get(id))
+      );
+    }
+  }
+
+  // Pick the request accordingly
+  submit(){
+    if (this.action == "Approve"){
+      this.approveRequests();
+    } else {
+      this.denyRequests();
+    }
+  }
+
+  // Deny the selected requests
+  denyRequests(): void {
+    for (let i: number = 0; i < this.selected.length; i++){
+      this.requestService.remove(this.selected[i]["id"]);
+    }
+    
+    this.requests = REQUESTS;
+    this.filteredRequests = this.performFilter("pending");
+    this.selected = [];
+
+  }
+
+  // Deny the selected requests
+  approveRequests(): void {
+    for (let i: number = 0; i < this.selected.length; i++){
+      this.requestService.supervisorApprove(this.selected[i]["id"]);
+    }
+    
+    this.requests = REQUESTS;
+    this.filteredRequests = this.performFilter("pending");
+    this.selected = [];
+
+  }
 
   constructor(private requestService: RequestService) {
     this.requests = REQUESTS;
-    this.filteredRequests = this.performFilter(2)
+    this.filteredRequests = this.performFilter("pending");
+    this.selected = [];
+    this.action = "Approve";
   }
 
   ngOnInit(): void {
